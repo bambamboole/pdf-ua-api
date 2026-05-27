0f92b1fb9ec945c8fc88d8aef6a7c08a276242e4
Manuel Christlieb <manuel@christlieb.eu>
2026-05-27 16:48:36 +0200

    feat: add template document and render request models

---
+++ app/src/main/kotlin/bambamboole/pdf/api/models/template/Template.kt
@@ -0,0 +1,33 @@
+package bambamboole.pdf.api.models.template
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Row(val blocks: List<Block>)
+
+@Serializable
+data class PageNumbersConfig(
+    val enabled: Boolean = false,
+    val position: Align = Align.CENTER,
+)
+
+@Serializable
+data class PageConfig(
+    val format: PageFormat = PageFormat.A4,
+    val locale: String = "de_DE",
+    val margins: SpacingConfig = SpacingConfig(20, 20, 20, 25),
+    val pageNumbers: PageNumbersConfig = PageNumbersConfig(),
+)
+
+@Serializable
+data class TemplateConfig(
+    val page: PageConfig = PageConfig(),
+    val typography: TypographyConfig = TypographyConfig(),
+)
+
+@Serializable
+data class Template(
+    val version: Int,
+    val config: TemplateConfig = TemplateConfig(),
+    val rows: List<Row> = emptyList(),
+)
